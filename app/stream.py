78bf96b0f49692beb78aec0819eeb495aa5611c0
Rohit Jain <rohit.jain@shadowfax.in>
2017-02-12 21:06:45 +0530

    insta stream added

--- app/stream.py
+++ app/stream.py
@@ -5,7 +5,7 @@ import datetime
 class StreamSocial():
 
     def __init__(self):
-        self.twitterApiObj, self.instaAccessToken = oauthsocial.TwitterAuth()._getAPIObj()
+        self.twitterApiObj, self.instaApiObj = oauthsocial.SocialAuth()._getAPIObj()
 
     def process_tweets(self, status,mode):
         result = {}
@@ -34,14 +34,67 @@ class StreamSocial():
             if last_id > sinceid:
                 sinceid = last_id
             results.append(status_process)
-            if len(results) == results_number:
+            if len(results) >= results_number:
                 break
 
         return results,sinceid
+
+
+    def process_insta(self, status, mode):
+        results = {}
+        user = status.get("user",{})
+        try:
+            date_created = status.get("taken_at","")
+            date_created = datetime.datetime.fromtimestamp(int(date_created)).strftime('%Y-%m-%d %H:%M:%S')
+        except:
+            date_created = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        try:
+            media_link = status["images"]["standard_resolution"]["url"]
+        except:
+            media_link = ""
+            pass
+        try:
+            caption = status["caption"]["text"]
+        except:
+            caption = ""
+
+        result["mediaLink"]        = media_link
+        result["includMedia"]      = 1
+        result["content"]          = caption
+        result["userDisplay"]      = user.get("profile_picture","")
+        result["screenName"]       = user.get("username","")
+        result["name"]             = user.get("full_name","")
+        result["displayFlag"]      = mode
+        result["time"]             = date_created
+        result["linkOriginalPost"] = "https://www.instagram.com/p/{insta_post}/".format(insta_post = status.get("code","")
+        result["type"]             = "insta"
+
+    def instaSearch(self, search, mode, location, sinceid, old_post, results_number):
+        results = []
+        search = search.split("#")[1] if "#" in search else search
+        instaPosts = api.feed_tag(q=search)
+        if "items" in instaPosts:
+            posts = instaPosts["items"]
+            try:
+                for post in posts:
+                    post_code = post["code"]
+                    if not old_post or (old_post and post_code not in sinceid):
+                        status_process = self.process_insta(post, mode)
+                        results.append(status_process)
+                    if post_code not in sinceid:
+                        sinceid.append(post_code)
+                    if results_number and (len(results) >= results_number):
+                        break
+            except:
+                pass
+        return results, sinceid
+        
         
     def _getSearchResults(self, search,start_date, end_date, mode, location,type_search, sinceid,old_post, results_number):
         if type_search == "tweets":
             return self.twitterSearch(search,start_date, end_date, mode, location, sinceid,old_post,results_number)
+        if type_search == "insta" and self.instaApiObj:
+            return self.instaSearch(search, mode, location, sinceid, old_post, results_number)
         return [], 0
 
 
